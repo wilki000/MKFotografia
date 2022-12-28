dafd5c30441a1966b61411047d4018a29437fae8
Kinga <kinga.wilczewska@gmail.com>
2022-12-28 19:45:38 +0100

    [#10][#11] Navigation component
    visual effects

--- src/app/components/navigation/nav-item/nav-item.component.ts
+++ src/app/components/navigation/nav-item/nav-item.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  HostListener,
+  AfterViewInit,
+} from '@angular/core';
 import { NavItemModel } from '@models/nav-item-model';
 import sharpArrowDropDown from '@iconify/icons-ic/sharp-arrow-drop-down';
 
@@ -7,14 +15,53 @@ import sharpArrowDropDown from '@iconify/icons-ic/sharp-arrow-drop-down';
   templateUrl: './nav-item.component.html',
   styleUrls: ['./nav-item.component.scss'],
 })
-export class NavItemComponent implements OnInit {
+export class NavItemComponent implements OnInit, AfterViewInit {
   @Input() mainCategory!: NavItemModel;
 
   arrowDrop = sharpArrowDropDown;
+  subcategories!: HTMLElement;
+  category!: HTMLElement;
 
   hasSubcategories!: boolean;
-  constructor() {}
+  constructor(private renderer: Renderer2, private elem: ElementRef) {}
+
   ngOnInit(): void {
     this.hasSubcategories = this.mainCategory.subCategories != null;
+  }
+
+  ngAfterViewInit(): void {
+    this.subcategories =
+      this.elem.nativeElement.querySelectorAll('.subcategory')[0];
+    this.category = this.elem.nativeElement.querySelectorAll('.category')[0];
+  }
+
+  toggleClickSubcategories(): void {
+    this.category.classList.toggle('clicked-subcategory');
+    this.subcategories.classList.toggle('clicked');
+    this.toggleHidden();
+  }
+
+  removeClick(force: boolean = false) {
+    if (this.subcategories === undefined || this.category === undefined) {
+      return;
+    }
+    if (force || window.innerWidth > 992) {
+      this.subcategories.classList.remove('clicked');
+      this.category.classList.remove('clicked-subcategory');
+      this.toggleHidden();
+    }
+  }
+
+  toggleHidden() {
+    if (this.subcategories.classList.contains('clicked')) {
+      this.subcategories.classList.remove('hidden');
+    } else {
+      this.subcategories.classList.add('hidden');
+    }
+  }
+
+  @HostListener('window:scroll')
+  hideSubcategory() {
+    this.removeClick(true);
   }
 }
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavItemComponent } from './nav-item/nav-item.component';
 import { NavItemModel, CATEGORIES } from '@models/nav-item-model';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   categories: NavItemModel[];
+  scrollPosition: number = 0;
+
   constructor() {
     this.categories = CATEGORIES;
   }
+  @HostListener('window:scroll', ['$event'])
+  setScrollPosition(event: MouseEvent) {
+    const oldPosition = this.scrollPosition;
+    this.scrollPosition = window.pageYOffset;
+    if (oldPosition < this.scrollPosition) {
+      document.getElementById('navbar')!.classList.remove('visible');
+      document.getElementById('navbar')!.classList.add('hidden');
+      console.debug('Scrolling down');
+    } else {
+      document.getElementById('navbar')!.classList.remove('hidden');
+      document.getElementById('navbar')!.classList.add('visible');
+      console.debug('scrolling up');
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  setScreenHeight(event: Event) {
+    this.screenHeight = window.innerHeight;
+  }
   toggleScroll() {
-    document.querySelector("body")?.classList.toggle("noscroll");
+    document.querySelector('body')?.classList.toggle('noscroll');
+  }
+
+  public screenHeight: any;
+
+  ngOnInit() {
+    this.screenHeight = window.innerHeight;
   }
 }
