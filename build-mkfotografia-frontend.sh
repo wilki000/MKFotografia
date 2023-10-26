fe5f12de6121d2a10732c3b4c8a816001f715f87
Kinga <kinga.wilczewska@gmail.com>
2023-10-26 20:14:27 +0200

    Banner - partnership

--- build-mkfotografia-frontend.sh
+++ build-mkfotografia-frontend.sh
@@ -1,1 +1,1 @@
-docker build -t mkfotografia-frontend:1.0.0 -t mkfotografia-frontend:latest -f mkfotografia-frontend.Dockerfile .
+docker build -t mkfotografia-frontend:1.0.1 -t mkfotografia-frontend:latest -f mkfotografia-frontend.Dockerfile .
